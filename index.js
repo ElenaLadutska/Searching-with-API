2d6c5c152cbfd04fd202583d42c2ae442617a442
Elena Ladutska <meduskalena@gmail.com>
2022-11-09 21:40:02 +0300

    fixed mistakes

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const searchImagesBtn = document.getElementById('search-images-btn');
+const searchImageBtn = document.getElementById('search-images-btn');
 const input = document.getElementById('searching-input');
-let content = document.getElementsByClassName('content')[0];
+const content = document.getElementsByClassName('content')[0];
+
+const sendRequestAndClearInput = () => {
+  sendRequest();
+
+  input.value = '';
+};
 
 const sendRequest = () =>{
   const requestURL = `https://api.unsplash.com/search/photos?query='+${input.value}+'&per_page=30&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`;
@@ -16,16 +22,14 @@ const sendRequest = () =>{
 }
 
 const showImages = (data) => {
-  // while (content.firstChild) {
-  //   content.removeChild(content.firstChild)
-  // };
-  //or
   content.textContent ='';
 
   for (let i = 0; i < data.results.length; i++) {
-    let image = document.createElement('div');
+    const image = document.createElement('div');
+
     image.className = 'image';
-    image.style.backgroundImage = "url("+data.results[i].urls.raw + "&w=1366&h=768" +")";
+    image.style.backgroundImage = `url(${data.results[i].urls.raw}&w=1366&h=768)`;
+
     content.appendChild(image);
 
     image.addEventListener('dblclick', function() {
@@ -34,14 +38,12 @@ const showImages = (data) => {
   }
 };
 
-searchImagesBtn.addEventListener('click', function(){
-  sendRequest();
-  input.value = '';
+searchImageBtn.addEventListener('click', function() {
+  sendRequestAndClearInput();
 });
 
 input.addEventListener('keydown', event => {
-  if( event.code === 'NumpadEnter' || event.code === 'Enter') {
-    sendRequest();
-    input.value = '';
+  if (event.code === 'NumpadEnter' || event.code === 'Enter') {
+    sendRequestAndClearInput();
   };
 });
